6358fbf2f5df1c53267112af06a4892c4738a1fe
Meen Joy <munibkhan7737@gmail.com>
2017-09-09 10:10:27 -0700

    kjs

--- model.js
+++ model.js
@@ -2,7 +2,8 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
-    name: String
+    name: String,
+    date: String
 })
 
 var patientData = mongoose.model("Patient", userSchema)
